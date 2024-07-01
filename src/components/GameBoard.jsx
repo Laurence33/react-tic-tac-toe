729002af807c3c0c2de0c9cb714543c81992eff1
Laurence Cortez <laurencecortez.com>
2024-07-02 07:36:44 +0800

    adding symbol to a box

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,17 +1,33 @@
-const board = [
+import { useState } from 'react';
+
+const initialBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialBoard);
+
+  function handleBtnClick(rowIndex, colIndex) {
+    if (gameBoard[rowIndex][colIndex] === null) {
+      setGameBoard((prevGameBoard) => {
+        const newGameBoard = Object.assign([], prevGameBoard);
+        newGameBoard[rowIndex][colIndex] = 'X';
+        console.log(newGameBoard);
+        return newGameBoard;
+      });
+    }
+  }
   return (
     <ol id="game-board">
-      {board.map((arr, i) => (
+      {gameBoard.map((arr, i) => (
         <li key={i}>
           <ol>
             {arr.map((playerIcon, iconIndex) => (
               <li key={iconIndex}>
-                <button>{playerIcon}</button>
+                <button onClick={() => handleBtnClick(i, iconIndex)}>
+                  {playerIcon}
+                </button>
               </li>
             ))}
           </ol>
